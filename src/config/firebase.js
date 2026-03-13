599d3692f0e75bfb1500bd4a46c01e619b5163e5
safvan <muhammedsafvanp96@gmail.com>
2026-03-13 05:43:19 +0530

    chore: add .env.example and configure firebase env variables

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,21 +1,21 @@
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCy9Vr4f_nllQ5ApzaEEO0LkFuK0MoesJ4",
-  authDomain: "olx-clone-70f4f.firebaseapp.com",
-  projectId: "olx-clone-70f4f",
-  storageBucket: "olx-clone-70f4f.firebasestorage.app",
-  messagingSenderId: "129345944346",
-  appId: "1:129345944346:web:c13081267f7e938f804aa3",
-  measurementId: "G-LKLWTLVK52"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
